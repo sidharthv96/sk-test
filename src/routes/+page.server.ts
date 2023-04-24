600d57dfc162e2b8f5fddd2f72ace5ef3d95870d
Sidharth Vinod <sidharthv96@gmail.com>
2023-04-24 20:50:17 +0530

    Init

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,7 @@
+export const actions = {
+	test: () => {
+		return {
+			hello: 'world'
+		};
+	}
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<form
+		method="POST"
+		action="?/test"
+		use:enhance={() => {
+			return ({ result, update }) => {
+				if (result.type === 'success') {
+					const x = result.data?.hello;
+					console.log({ x });
+				}
+				update();
+			};
+		}}
+	>
+		<input type="text" name="temp" id="temp" />
+	</form>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
